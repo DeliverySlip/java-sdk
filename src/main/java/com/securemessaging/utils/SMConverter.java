4ddd6b949c4ba770c5f23efc75830bba110531a8
bensoer <ben@soernet.ca>
2018-07-16 15:30:42 -0700

    implemented GetMessage route

---
+++ src/main/java/com/securemessaging/utils/SMConverter.java
@@ -0,0 +1,51 @@
+package com.securemessaging.utils;
+
+import com.securemessaging.sm.Message;
+import com.securemessaging.sm.enums.BodyFormat;
+import com.securemessaging.sm.enums.FyeoType;
+import com.securemessaging.sm.response.MessageSummaryResponse;
+
+public class SMConverter {
+
+
+    public static Message convertMessageSummaryToMessage(MessageSummaryResponse messageSummary){
+
+        Message message = new Message();
+        message.setMessageGuid(messageSummary.guid);
+
+        String[] bccList = new String[messageSummary.bcc.size()];
+        for(int i = 0; i < bccList.length; i++){
+            bccList[i] = messageSummary.bcc.get(i).email;
+        }
+        message.setBCC(bccList);
+
+        String[] ccList = new String[messageSummary.cc.size()];
+        for(int i = 0; i < ccList.length; i++){
+            ccList[i] = messageSummary.cc.get(i).email;
+        }
+        message.setCC(ccList);
+
+        String[] toList = new String[messageSummary.to.size()];
+        for(int i = 0; i < toList.length; i++){
+            toList[i] = messageSummary.to.get(i).email;
+        }
+        message.setTo(toList);
+
+        message.hasBeenSaved = true;
+        message.setFrom(new String[]{messageSummary.sender.email});
+        message.setBody(messageSummary.body);
+        message.setBodyFormat(BodyFormat.enumFromEnumText(messageSummary.bodyFormat));
+        message.setSubject(messageSummary.subject);
+        message.setTracking(messageSummary.messageOptions.allowTracking);
+        message.setShareTracking(messageSummary.messageOptions.shareTracking);
+
+        message.setForward(messageSummary.messageOptions.allowForward);
+        message.setReply(messageSummary.messageOptions.allowReply);
+        message.setFyeoType(FyeoType.enumFromEnumText(messageSummary.messageOptions.fyeoType));
+
+
+        //message.setPassword(messageSummary);
+
+        return message;
+    }
+}
---
+++ src/test/java/GetMessageTests.java
@@ -0,0 +1,38 @@
+import com.securemessaging.SecureMessenger;
+import com.securemessaging.ex.SecureMessengerClientException;
+import com.securemessaging.ex.SecureMessengerException;
+import com.securemessaging.sm.Credentials;
+import com.securemessaging.sm.Message;
+import com.securemessaging.sm.search.SearchMessagesResults;
+import com.securemessaging.sm.search.SearchMessagesFilter;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+public class GetMessageTests extends BaseTestCase {
+
+    @Test
+    public void testGetMessageFirst25() throws SecureMessengerException, SecureMessengerClientException{
+
+        SecureMessenger messenger = SecureMessenger.resolveViaServiceCode(serviceCode);
+        Credentials credentials = new Credentials(username, password);
+        messenger.login(credentials);
+
+        SearchMessagesFilter filter = new SearchMessagesFilter();
+
+        SearchMessagesResults results = messenger.searchMessages(filter);
+        Iterator<Message> iterator = results.iterator();
+
+        for(int i = 0; i < 25; i++){
+            Message message = iterator.next();
+
+            Message gotAgain = messenger.getMessage(message.getMessageGuid());
+
+            if(!message.getMessageGuid().equals(message.getMessageGuid())){
+                Assert.fail();
+            }
+        }
+    }
+
+}
